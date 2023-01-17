7661f2c9cda27fe6b457c10065debf44a80b1d00
lina-22 <haquefahinur@gmail.com>
2023-01-17 21:56:10 +0100

    ScrollView concept clear

---
+++ DoneWithIt/ScrollView/ScrollView.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+
+class ScrollView extends Component {
+    render() {
+        return (
+          <ScrollView horizontal={true}>
+            <View style={{backgroundcolor: 'red',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'black',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'green',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'blue',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'red',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'black',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'green',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'blue',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'red',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'black',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'green',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'blue',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'red',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'black',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'green',height:100,width:100}}></View>
+            <View style={{backgroundcolor: 'blue',height:100,width:100}}></View>
+            </ScrollView>
+            
+        );
+    }
+}
+
+export default ScrollView;
